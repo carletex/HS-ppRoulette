9d13241080f2adcd289ae0c50c03f0788a42bc0c
Victor Felder <victorfelder@gmail.com>
2014-09-29 16:25:52 -0400

    Add bower, oauth2 WIP

--- client/app.js
+++ client/app.js
@@ -3,14 +3,21 @@
 var app = angular.module('ppRouletteApp', ['ngRoute', 'satellizer']);
 
 app.config(function($authProvider) {
-
-    $authProvider.hackerschool({
-      clientId: '0dcf15561cd59ddd398c74118fc24ddfb1670cfeb3fa0816a540d68ae5df7c85'
-    });
-
+  $authProvider.oauth2({
+    url: '/auth/hackerschool',
+    clientId: '0dcf15561cd59ddd398c74118fc24ddfb1670cfeb3fa0816a540d68ae5df7c85',
+    authorizationEndpoint: 'https://www.hackerschool.com/oauth/authorize',
+    redirectUri: window.location.origin,
+    scope: [],
+    scopeDelimiter: ' ',
+    display: 'popup',
+    type: '2.0',
+    popupOptions: { width: 1000, height: 500 }
   });
+});
 
-app.controller("SessionList", function($scope) {
+
+app.controller("SessionList", function($scope, $auth) {
 
   $scope.authenticate = function(provider) {
       $auth.authenticate(provider);
