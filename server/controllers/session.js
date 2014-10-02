cb0726ce5ec95581dffde14dd2abd1d4f95e9141
Victor Felder <victorfelder@gmail.com>
2014-10-02 12:06:23 -0400

    session list

--- server/controllers/session.js
+++ server/controllers/session.js
@@ -84,11 +84,40 @@ module.exports.assignRandomSession = function(req, res) {
 };
 
 module.exports.getSessionsStatus = function(req, res) {
-  var now = new Date();
-  var end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 18, 30);
-  Session.find({date: {$gte: now, $lt: end}, hostId: {$ne: req.hsId}, guestId: -1})
+  var now = moment();
+  var end = now.hours(18).minutes(30);
+  Session.find({
+    date: {
+      $gte: now.toISOString(), $lt: end.toISOString()
+    },
+    hostId: {
+      $ne: req.hsId
+    },
+    guestId: -1
+  })
     .distinct('hostId')
     .count(function(err, count) {
       res.json(count);
+    });
+};
+
+
+module.exports.listSessions = function(req, res) {
+  var now = moment();
+  var end = now.hours(18).minutes(30).add(1, 'day');
+  Session.find({
+    date: {
+      $gte: now.toISOString(),
+      $lt: end.toISOString()
+    },
+    hostId: {
+      $ne: req.hsId
+    },
+    guestId: {
+      $ne: -1
+    }
+  })
+    .distinct('hostId').exec(function(err, sessions) {
+      console.log(sessions);
     });
 };
