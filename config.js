46fb2ae863ba57b02f64bf1522b19c7eaf3c3951
Carlos Sánchez <oceanrdn@gmail.com>
2014-10-19 18:46:52 +0200

    Changed default value for MONGO_URI in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   TOKEN_SECRET: process.env.TOKEN_SECRET,
-  MONGO_URI: process.env.MONGO_URI || 'localhost',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/pproulette',
   HS_SECRET: process.env.HS_SECRET,
   ZULIP_EMAIL: process.env.ZULIP_EMAIL,
   ZULIP_SECRET: process.env.ZULIP_SECRET
